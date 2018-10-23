d6eab454b192ddaa4a6edb8754a49e047790cfe6
daisymacharia <daisy.macharia@andela.com>
2018-10-23 16:02:43 +0300

    Delete items from the server and client cache

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
+import DeleteItem from "./DeleteItem";
+import formatMoney from "../lib/formatMoney";
+
 import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
-import Link from "next/link";
-import formatMoney from "../lib/formatMoney";
 
 class Item extends Component {
 	static propTypes = {
@@ -39,7 +41,7 @@ class Item extends Component {
 						<a>Edit</a>
 					</Link>
 					<button> Add To Cart</button>
-					<button> Delete </button>
+					<DeleteItem id={item.id}> Delete This Item</DeleteItem>
 				</div>
 			</ItemStyles>
 		);
